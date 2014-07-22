3ef5c7eccfe8d6844a79b07d7150f2f86babbb36
Pieter Carette <pieter@siteoptimo.com>
2014-07-22 14:21:48 +0200

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Plivo SMS notifications ===
+Contributors: pieterc,vdwijngaert
+Donate link: http://www.siteoptimo.com/
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+Tags: plivo, sms, notifications, support, update, cart, woocommerce, status, shipping
+Requires at least: 3.6
+Tested up to: 3.9.1
+Stable tag: 1.5.4.2
+
+Allow your WooCommerce website to send SMS notifications to your customers.
+
+== Description ==
+
+Description here
+
+= Titel =
+Title here
+
+== Installation ==
+
+1. Upload the `woocommerce-plivo` folder to the `/wp-content/plugins/` directory
+1. Activate the Woocommerce Plivo plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+You'll find the [FAQ on siteoptimo.com](http://www.siteoptimo.com/wordpress/plugins/).
---
+++ woocommerce-plivo.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: WooCommerce Plivo
+Version: 1.0
+Plugin URI: http://www.siteoptimo.com/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wcplivoplugin
+Description: Send SMS update notifications to your customers with this Plivo plugin for WooCommerce.
+Author: SiteOptimo
+Author URI: https://www.siteoptimo.com/
+Text Domain: woocommerce-plivo
+Domain Path: /i18n/languages/
+License: GPL v3
+
+WordPress SEO Plugin
+Copyright (C) 2014, SiteOptimo - team@siteoptimo.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+gi
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Check if WooCommerce is active
+ */
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+}
