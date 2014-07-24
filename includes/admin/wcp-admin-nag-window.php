e95735b2b8a23e1cdfddd909f7cf22a371cd152c
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2014-07-24 10:16:28 +0200

    Fixes ability to dispose of the admin notice about missing configuration.

--- includes/admin/wcp-admin-nag-window.php
+++ includes/admin/wcp-admin-nag-window.php
@@ -7,8 +7,8 @@ class WCP_Admin_Nag_Window
 {
     public function __construct()
     {
-        add_action('admin_notices', array($this, 'wcp_admin_notice'));
         add_action('admin_notices', array($this, 'wcp_nag_ignore'));
+        add_action('admin_notices', array($this, 'wcp_admin_notice'));
 
     }
 
@@ -21,7 +21,10 @@ class WCP_Admin_Nag_Window
         if (!get_user_meta($user_id, 'wcp_ignored_notice', true) && (empty($wcp_auth_id) || empty($wp_auth_password))) {
 
             if (current_user_can('install_plugins') && !(isset($_GET['tab']) && $_GET['tab'] == 'woocommerce_sms_settings')) {
-                echo '<div class="updated"><p>' . __('The WooCommerce Plivo integration plugin is not yet configured.', 'woocommerce-plivo') . '<br><a href="admin.php?page=wc-settings&tab=woocommerce_sms_settings">' . __('Take me to the Plivo settings page!', 'woocommerce-plivo') . '</a> | <a href="?wcp_nag_ignore=0">'.__('I know, hide this message','woocommerce-plivo').'</a></p></div>';
+                $current_page = $_SERVER['REQUEST_URI'];
+                $separator = strpos($current_page, '?') !== false ? '&' : '?';
+                $nag_url = $current_page . $separator . 'wcp_nag_ignore';
+                echo '<div class="updated"><p>' . __('The WooCommerce Plivo integration plugin is not yet configured.', 'woocommerce-plivo') . '<br><a href="admin.php?page=wc-settings&tab=woocommerce_sms_settings">' . __('Take me to the Plivo settings page!', 'woocommerce-plivo') . '</a> | <a href="' . $nag_url . '">'.__('I know, hide this message','woocommerce-plivo').'</a></p></div>';
             }
 
         }
@@ -32,7 +35,7 @@ class WCP_Admin_Nag_Window
         global $current_user;
         $user_id = $current_user->ID;
         /* If user clicks to ignore the notice, add that to their user meta */
-        if (isset($_GET['wcp_nag_ignore']) && '0' == $_GET['wcp_nag_ignore']) {
+        if (isset($_GET['wcp_nag_ignore'])) {
             add_user_meta($user_id, 'wcp_ignored_notice', true, true);
         }
     }
