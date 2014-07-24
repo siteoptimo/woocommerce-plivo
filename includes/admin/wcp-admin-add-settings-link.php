7ade7c465c995b425ba6d97a0058312a820f3219
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2014-07-24 13:45:53 +0200

    Refactors the WCP_Admin_Add_Settings_Link class to its own file.

---
+++ includes/admin/wcp-admin-add-settings-link.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Koen Van den Wijngaert <koen@siteoptimo.com>
+ */
+if(!defined('ABSPATH')) exit;
+
+class WCP_Admin_Add_Settings_Link
+{
+
+    public function __construct()
+    {
+        add_filter('plugin_action_links_' . WCP_BASENAME, array($this, 'wcp_settings_link'));
+    }
+
+    public function wcp_settings_link($links)
+    {
+        $settings_link = '<a href="admin.php?page=wc-settings&tab=woocommerce_sms_settings">Settings</a>';
+        array_unshift($links, $settings_link);
+
+        return $links;
+    }
+
+}
