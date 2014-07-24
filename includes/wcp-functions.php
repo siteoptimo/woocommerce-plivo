4619ee2949ab40c7e52a6912403283bf6f98390a
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2014-07-24 13:51:21 +0200

    Adds check to see if the plugin is valid and loads classes accordingly.

--- includes/wcp-functions.php
+++ includes/wcp-functions.php
@@ -4,3 +4,8 @@
  */
 if(!defined('ABSPATH')) exit;
 
+function is_wcp_ready() {
+    $wcp_auth_id = get_option('wcp_auth_id');
+    $wp_auth_password = get_option('wcp_auth_password');
+    return !empty($wcp_auth_id) && !empty($wp_auth_password);
+}
